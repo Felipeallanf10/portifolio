bfa42b04a9651928433dd6f7328cdb7316c79c53
EA - Felipe Allan Nascimento Cruz <felipe_allan@educadventista.org>
2024-03-07 13:58:51 -0300

    First commit

---
+++ src/Home.tsx
@@ -0,0 +1,24 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        <header>
+          <div id="header-wrap-inner">
+            <h2>Felipe<strong id='ponto'>.</strong><strong id='underline'>_</strong></h2>
+            <nav id='nav-container'>
+              <ul>
+                <li>// home</li>
+                <li>// expertise</li>
+                <li>// work</li>
+                <li>// expertise</li>
+                <li>// contact</li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+    </div>
+  ) 
+}
+
+export default Home

701789d199575888fa2f48dcdc0d27eb5187d5dd
Felipe <felipeallanf10@outlook.com>
2024-04-03 22:26:44 -0300

    continuação das skills, commit para mostrar para o professor

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import './Home.css'
 import iconComputer from './assets/computer.svg'
 import iconReact from './assets/react.svg'
 
+
 function Home() {
   return (
     <div>
@@ -52,23 +53,63 @@ function Home() {
         <h2>My Expertise</h2>
 
         <div className="boxSkills">
-          <div className="itemSkills" id="boxSkills1">
+          <div className="itemSkills" id="boxSkills3">
             <div className="titleBox">
               <img src={iconComputer} alt="monitor" />
               <h3>
                 Software <br /> Development
               </h3>
             </div>
+            <div className="contentBox">
+              <div className="lineH3">
+                <div className="h3">{'<h3>'}</div>
+                <hr />
+                <div className="h3">{'</h3>'}</div>
+              </div>
+              <p>
+                Experienced in both functional and OOP: Dart, Python, Java,
+                JavaScript, TypeScript.
+              </p>
+            </div>
           </div>
           <div className="itemSkills" id="boxSkills2">
             <div className="titleBox">
               <img src={iconReact} alt="monitor" />
               <h3>
-                Frontend Dev <br /> React, NextJS{' '}
+                Frontend Dev <br /> React, NextJS
               </h3>
             </div>
+            <div className="contentBox">
+              <div className="lineH3">
+                <div className="h3">{'<h3>'}</div>
+                <hr />
+                <div className="h3">{'</h3>'}</div>
+              </div>
+              <p>
+                Passionate about UI/UX. Over 5 years of development experience
+                in HTML, CSS, JS, React and NextJS frameworks.
+              </p>
+            </div>
           </div>
-          <div className="itemSkills" id="boxSkills3"></div>
+          <div className="itemSkills" id="boxSkills3">
+            <div className="titleBox">
+              <img src={iconReact} alt="monitor" />
+              <h3>
+                Flutter Dev <br /> Android, iOS
+              </h3>
+            </div>
+            <div className="contentBox">
+              <div className="lineH3">
+                <div className="h3">{'<h3>'}</div>
+                <hr />
+                <div className="h3">{'</h3>'}</div>
+              </div>
+              <p>
+                Skilled in developing hybrid mobile apps and cross-platform
+                solutions using the Flutter framework.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
